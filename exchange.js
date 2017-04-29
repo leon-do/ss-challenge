7539c63ff40b0682aa708aa8dff0888b1a8df3df
Leon Do <leondo123@gmail.com>
2017-04-29 15:15:56 -0400

    comments added

--- exchange.js
+++ exchange.js
@@ -1,9 +1,27 @@
-// This code does API calls to all of the exchanges and saves the rates into an object called results
+// This code does API calls to all of the exchanges and saves the rates into an array called lowestAsk
+
+/*
+
+    The flow:
+    call out to poloniex and retrieve data1
+    call out to Bittrex and retrieve data2
+    the data is then stored in an object called results = {data1, data2}
+    results is then parsed and pushed into an array called lowestAsk = [ { rate: 0.05247999, name: 'poloniex' }, { rate: 0.05247765, name: 'bittrex' } ]
+    lowestAsk passed back in a callback for use
+
+    Pros of this structure: 
+        - Easy to scale by adding more API calls inside async.parallel
+        - (relatively) easy to understand
+
+    Cons of this structure:
+        - not performant. Some data structures can be retrieved in one call. This method may call the same data structure multiple times.
+
+*/
 
 var async = require('async')
 var request = require('request')
 
-//expose results to other js files
+//expose results to other js files (server.js)
 exports.rates = function(coin1, coin2, cb){
     
     // run the API calls in parallel
@@ -25,13 +43,31 @@ exports.rates = function(coin1, coin2, cb){
 
             // call to get Bittrex ticker
             request(`https://bittrex.com/api/v1.1/public/getticker?market=${coin1}-${coin2}`, function (error, response, body) {
-                callback(error, body)
+                var object = JSON.parse(body)
+                callback(error, object)
             })
 
 
         }
+
+
+
+        /* 
+        ADD ANOTHER API CALL HERE
+
+        exchangeZ : function(callback){
+            request(`https://SOME_URL_HERE`, function (error, response, body) {
+                callback(error, body)
+            })
+        }
+
+        */
+
+
+
+
     },
-    // combines two api calls into an object called results
+    // combines all of the api calls into an object called results
     function(error, results) {
         // parse the results into a array called lowestAsk. I'm using an array to get the lowest value in O(n) time. This data structure can be optimized
         var lowestAsk = []
@@ -39,7 +75,7 @@ exports.rates = function(coin1, coin2, cb){
             rate: parseFloat(results.poloniex.lowestAsk),
             name: "poloniex"})
         lowestAsk.push({
-            rate: JSON.parse(results.bittrex).result.Ask,
+            rate: parseFloat(results.bittrex).result.Ask,
             name: "bittrex"})
         // lowestAsk = [ { rate: 0.05247999, name: 'poloniex' }, { rate: 0.05247765, name: 'bittrex' } ]
         cb(lowestAsk)
--- server.js
+++ server.js
@@ -2,16 +2,32 @@ var exchange = require('./js/exchange.js')
 var bestRate = require('./js/bestRate.js')
 
 
-// calls a funciton from exchange.js
+/* 
+
+    The flow:
+    exchange.rates for BTC and ETH will return array = [ { rate: 0.05247999, name: 'poloniex' }, { rate: 0.05247765, name: 'bittrex' } ]
+    passing the array through the function bestRate will return the best rate out of that array { rate: 0.05247765, name: 'bittrex' }
+
+    exchange is a funciton from exchange.js
+    exchange takes in two arguments, coin1 and coin2 and returns an array with different rates from different exchanges.
+    The different exchanges are poloniex and bittrex. exchange.js is built to scale
+
+
+*/
+
+
+
+
 exchange.rates('BTC','ETH', function(arr){
-    // calls funtion from bestRate.js to get back an array arr
+    // bestRate calls funtion from bestRate.js 
+    // bestRate takes in an array as an argument and returns the index with the lowest rate
     // arr = [ { rate: 0.05247999, name: 'poloniex' }, { rate: 0.05247765, name: 'bittrex' } ]
     bestRate.indexOfLowestVal(arr, function(index){
         //index from the callback is the index with the lowest rate
         console.log(`
 
             BTC to ETH exchange rate: ${JSON.stringify(arr)}
-            Between Poloniex and Bittrex, the lowest ask is: ${arr[index].name}
+            Between Poloniex and Bittrex, the lowest ask is: ${arr[index].name} at ${arr[index].rate}
 
         `)
     })
@@ -23,7 +39,7 @@ exchange.rates('BTC','LTC', function(arr){
 
             BTC to LTC exchange rate: ${JSON.stringify(arr)}
 
-            Between Poloniex and Bittrex, the lowest ask is: ${arr[index].name}
+            Between Poloniex and Bittrex, the lowest ask is: ${arr[index].name} at ${arr[index].rate}
 
         `)    
     })
@@ -35,7 +51,7 @@ exchange.rates('BTC','DASH', function(arr){
 
             BTC to DASH exchange rate: ${JSON.stringify(arr)}
 
-            Between Poloniex and Bittrex, the lowest ask is: ${arr[index].name}
+            Between Poloniex and Bittrex, the lowest ask is: ${arr[index].name} at ${arr[index].rate}
 
         `)    
     })
