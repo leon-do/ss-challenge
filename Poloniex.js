a260a7948dbc1d5128f69735269e6ceed901fa2a
Leon Do <leondo123@gmail.com>
2017-04-29 01:03:43 -0400

    name change

--- Poloniex.js
+++ Poloniex.js
@@ -12,15 +12,22 @@ var connection = new autobahn.Connection({
 // when the connection is open...
 connection.onopen = function (session) {
 
+        // subscribe to market event
         function marketEvent (args,kwargs) {
+                //display real time market events
                 console.log(args);
         }
+
+        //subscribe to ticker event
         function tickerEvent (args,kwargs) {
+                //display real time ticker events 
+                // args = [currencyPair, last, lowestAsk, highestBid, percentChange, baseVolume, quoteVolume, isFrozen, 24hrHigh, 24hrLow]
                 console.log(args);
         }
+
         // session.subscribe('BTC_ETH', marketEvent);
         // session.subscribe('ticker', tickerEvent);
-        
+
 }
  
 connection.onclose = function () {
