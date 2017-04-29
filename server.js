f2b30dc0037d3953034301d3bf1c11b2bc566b89
Leon Do <leondo123@gmail.com>
2017-04-29 05:01:33 -0400

    exchange.js is complete

--- server.js
+++ server.js
@@ -1,17 +1,5 @@
-var bittrex = require('./bittrex.js')
-var poloniex = require('./poloniex.js')
-var ticker = require('./ticker.js')
+var exchange = require('./exchange.js')
 
-
-
-// bittrex.rate('BTC-DASH', function(data){
-//     console.log(data)
-// })
-
-// poloniex.rate('BTC_DASH', function(data){
-//     console.log(data)
-// })
-
-ticker.rate(function(data){
-    console.log(data.poloniex)
+exchange.rates('BTC-ETH', function(data){
+    console.log(data)
 })
