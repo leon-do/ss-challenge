8718fdcfec7b01df22aa5b660603bec5de12572a
Leon Do <leondo123@gmail.com>
2017-05-05 12:07:57 -0400

    client side es5 for usability

--- server/experiments/bittrex.js
+++
@@ -1,9 +0,0 @@
-// https://www.npmjs.com/package/node.bittrex.api
-
-var request = require('request');
-
-
-var pair = 'BTC-ETH'
-request(`https://bittrex.com/api/v1.1/public/getticker?market=${pair}`, function (error, response, body) {
-    console.log(body)
-})
