3d87af482eef33d34ce81e6b8bb23b5ec174c161
Leon Do <leondo123@gmail.com>
2017-05-06 12:57:35 -0400

    promises complete with comments

--- server/js/bestExchange.js
+++ server/js/bestExchange.js
@@ -1,17 +1,15 @@
 /* 
 
     The flow:
-    exchange.rates for BTC and ETH will return array = [ { rate: 0.05247999, name: 'poloniex' }, { rate: 0.05247765, name: 'bittrex' } ]
-    passing the array through the function bestRate will return the best rate out of that array { rate: 0.05247765, name: 'bittrex' }
-
-    exchange is a funciton from exchange.js
-    exchange takes in two arguments, coin1 and coin2 and returns an array with different rates from different exchanges.
+    get all of the rates from allRates.js
+    sort through allRates and find the indexOf the lowest rate
+    store it into const BTC_ETC, BTC_LTC and BTC_DASH
+    Promise.all([BTC_ETC, BTC_LTC, BTC_DASH]) stores the coin pairs into a variable called bestExchange
+    export bestExchange for server.js
 
 */
 "use strict";
-const async = require('async')
 const allRates = require('./allRates.js')
-const bestRate = require('./bestRate.js')
 
 //expose results to other js files (server.js)
 exports.lowestRate = (cb) => {
@@ -19,56 +17,60 @@ exports.lowestRate = (cb) => {
     const BTC_ETC = new Promise((resolve, reject) => {
         // get all rates for all exchanges
         allRates.rates('BTC','ETH', (arr) => {
-            // bestRate calls function from bestRate.js 
-            // bestRate takes in an array as an argument and returns the index with the lowest rate
+
             // arr = [ { rate: 0.05247999, name: 'poloniex' }, { rate: 0.05247765, name: 'bittrex' } ]
-            bestRate.indexOfLowestVal(arr, (index) => {
-                //index from the callback is the index with the lowest rate
-                console.log(`
+            // minIndex finds the indexOf the lowest rate
 
-                    BTC to ETH exchange rate: ${JSON.stringify(arr)}
-                    Between Poloniex, Bittrex and Yobit, the lowest ask is: ${arr[index].name} at ${arr[index].rate}
+            const minIndex = arr.indexOf(arr.reduce((a,b) => a.rate < b.rate ? a : b))
 
-                `)
+            console.log(`
 
-            //store data back into results (at the bottom)
-            resolve({exchangeName: arr[index].name, rate: arr[index].rate})
-            })
-        })        
+                BTC to ETH exchange rate: ${JSON.stringify(arr)}
+                Between Poloniex, Bittrex and Yobit, the lowest ask is: ${arr[minIndex].name} at ${arr[minIndex].rate}
+
+            `)
+
+            resolve({exchangeName: arr[minIndex].name, rate: arr[minIndex].rate})
+        })
     })
 
 
 
     const BTC_LTC = new Promise((resolve, reject) => {
         allRates.rates('BTC','LTC', (arr) => {
-            bestRate.indexOfLowestVal(arr, (index) => {
-                console.log(`
+            
+            const minIndex = arr.indexOf(arr.reduce((a,b) => a.rate < b.rate ? a : b))
+           
+            console.log(`
 
-                    BTC to LTC exchange rate: ${JSON.stringify(arr)}
-                    Between Poloniex, Bittrex and Yobit, the lowest ask is: ${arr[index].name} at ${arr[index].rate}
+                BTC to LTC exchange rate: ${JSON.stringify(arr)}
+                Between Poloniex, Bittrex and Yobit, the lowest ask is: ${arr[minIndex].name} at ${arr[minIndex].rate}
 
-                `) 
-            resolve({exchangeName: arr[index].name, rate: arr[index].rate})
-            })
+            `) 
+
+            resolve({exchangeName: arr[minIndex].name, rate: arr[minIndex].rate})
         })
     })
 
 
     const BTC_DASH = new Promise((resolve, reject) => {
         allRates.rates('BTC','DASH', (arr) => {
-            bestRate.indexOfLowestVal(arr, (index) => {
+
+            const minIndex = arr.indexOf(arr.reduce((a,b) => a.rate < b.rate ? a : b))
+
                 console.log(`
 
                     BTC to DASH exchange rate: ${JSON.stringify(arr)}
-                    Between Poloniex, Bittrex and Yobit, the lowest ask is: ${arr[index].name} at ${arr[index].rate}
+                    Between Poloniex, Bittrex and Yobit, the lowest ask is: ${arr[minIndex].name} at ${arr[minIndex].rate}
 
                 `)   
-            resolve({exchangeName: arr[index].name, rate: arr[index].rate})
-            })
+
+            resolve({exchangeName: arr[minIndex].name, rate: arr[minIndex].rate})
         })
     })
 
 
+    // Promise.all stores all of the lowest rates into a variable called bestExchange
     Promise.all([BTC_ETC, BTC_LTC, BTC_DASH])
     .then(results => {
         // results = [ { exchangeName: 'yobit', rate: 0.05726615 },{ exchangeName: 'yobit', rate: 0.0157702 }, { exchangeName: 'yobit', rate: 0.061504 } ]
